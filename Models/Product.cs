22097f38694f326068401575ac0df4a2c5fec058
shymanskyidenys <shymanskyi.denys@stud.onu.edu.ua>
2026-03-28 00:10:42 +0200

    Added attribute information to the Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,12 +1,21 @@
-namespace PcStoreApp.Models
+namespace PcStoreApp.Models;
+
+public class Product
 {
-    public class Product
-    {
-        public int ProductId { get; set; }
-        public string Name { get; set; } = "";
-        public decimal Price { get; set; }
-        public string Description { get; set; } = "";
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; } = "";
-    }
+    public int ProductId { get; set; }
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+    public string Description { get; set; } = "";
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; } = "";
+
+    public List<ProductAttributeInfo> Attributes { get; set; } = [];
+
+    public List<Product> Components { get; set; } = [];
+}
+
+public class ProductAttributeInfo
+{
+    public string AttributeName { get; set; } = String.Empty;
+    public string AttributeValue { get; set; } = String.Empty;
 }
